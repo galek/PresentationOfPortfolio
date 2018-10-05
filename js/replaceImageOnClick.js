5b729d9deddff44ab397400c4c738d301d00a8fe
Nick Galko <galek@users.noreply.github.com>
2018-10-06 00:54:29 +0300

    Final code
    add in images directory images (currently is max 8 images)
    Created for 1 hour for one company

---
+++ js/replaceImageOnClick.js
@@ -0,0 +1,17 @@
+﻿var StartIterator = 1;
+var iterator = StartIterator;
+var maxElement = 8; 
+/* 
+maxElement - сколько в папке рисунков
+*/
+
+function Replace(image)
+{
+	iterator++;
+	
+	if(iterator > maxElement)
+		iterator = StartIterator;
+	
+	image.src = "images/"+ iterator +".jpg";
+	window.scrollTo(0, 0); // change scrolling on up
+}
